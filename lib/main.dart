3f4223a5a6ee9dcfbdb95774feca5345a3e1bf9f
Takeyuki UEDA <ueda@latelierdueda.com>
2019-10-03 10:45:39 +0900

    lesson #10

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,10 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.teal[900],
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            print("you press me");
-          },
-          child: Text('click me'),
-        ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        color: Colors.grey[400],
+        child: Text('hello'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
